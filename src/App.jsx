8fa81bf3eaa9e77dea63ef609666bd7258680c41
xiaochenyeye <1223300926@qq.com>
2024-09-29 22:04:55 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import Layout from './layout/index.jsx'
+function App() {
+
+  return (
+    <>
+      <Layout />
+    </>
+  )
+}
+
+export default App
---
+++ src/layout/index.jsx
@@ -0,0 +1,5 @@
+export default function Layout(){
+    return <>
+    <div className="w-screen h-screen ">1111</div>
+    </>
+}
